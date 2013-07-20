9a798c234d133bf88d09d05904c66b5ded6ce3e4
Steve Donie <steve@donie.us>
2013-07-20 00:22:46 -0500

    lots of new stuff

--- tracker-text.js
+++ tracker-text.js
@@ -1,60 +1,158 @@
-﻿var workers = { 	"Julie" : { nameChoices: "Julie, Julie Donie", number: "15126190419" },
-					"Steve" : { nameChoices: "Steve, Steve Donie",    number: "15127977822" },
-					"Alexa" : { nameChoices: "Alexa, Alexa Villalobos",    number: "12176498591" } };
+﻿var employees = { 	"Julie" : { fullName: "Julie Donie",      number: "15126190419", roles: ["Admin","Supervisor"] },
+					"Steve" : { fullName: "Steve Donie",      number: "15127977822", roles: ["Admin"] },
+					"Alexa" : { fullName: "Alexa Villalobos", number: "12176498591", roles: ["Admin","Supervisor"] } 
+			};
 
-// -----------
-// turn the contacts into a comma separated list of options for each contact (simple grammar)
-function listOptions( theContacts )
-{
-  var s ='';
-  for( var contact in theContacts )
-  {
-    if (s != '') { s = s + ", " };
-    s = s + contact + " (" + theContacts[ contact ].nameChoices + ")";
-  }
-  return s;
-}
+var clients = ["Binder","Donie"];
 
-function lookupWorker(phoneNumber) {
-	for (var worker in workers) {
-		if (workers[worker].number == phoneNumber) {
-			return worker;
+function lookupEmployee() {
+	for (var employee in employees) {
+		if (employees[employee].number == currentCall.callerID) {
+			return employee;
 		}
 	}
 	return null;
 }
 
 function handleTextDirectInput() {
-	var result = ask("Reply with clientname, hours, and date (optional) i.e 'Binder 4.5' or 'Davenport 3.25 7-21' If you omit the date, it uses today's date.",
+	var result = ask("Reply with clientname, hours, and (optional) date. For example 'Binder 4.5' or 'Davenport 3.25 7-21' If you omit the date, it uses today's date.",
 	{
 		choices: "[ANY]",
 		timeout: 300.0,
 		onChoice: function (event) { 
-			say("Thanks - recorded " + event.value);
+			handleTimeEntry (event.value);
 		}
 	});
 }
 
+function handleKnownEmployee(employee) {
+	if (isValidTimeEntry(currentCall.InitialText)) {
+		handleTimeEntry(currentCall.InitialText);
+	} else {
+		handleTextDirectInput();
+	}
+}
+
+function isValidTimeEntry(text) {
+	var parts = timeEntryString.split(" ");
+	if (parts.length < 2 || parts.length > 3) {
+		return false;
+	}
+	var client = parts[0];
+	var hours = parts[1];
+	var date = "today";
+	if (parts.length == 3) {
+		date = parts[2];
+	}
+	if (isValidClient(client) && isValidHours(hours) && isValidDate(date)){
+		return true;
+	}
+	var message = "Something doesn't compute: ";
+	if (!isValidClient(client)){
+		message += "'" + client + "' is not a known client. ";
+	}
+	if (!isValidHours(hours)){
+		message += " I don't understand '" + hours + "' as an hours value. ";
+	}
+	if (!isValidDate(date)){
+		message += " I don't understand '" + date + "' as a date value.";
+	}
+	say(message);
+	return false;
+}
+
+function handleRegistration(newUserName) {
+	say ("Thanks for registering " + newUserName + ". I need to verify your registration.");
+	// send text to Supervisors, with new user name and number. If they confirm, save new user.
+}
+
+// This can either be two parts, (client and hours), or three parts (client, hours, date)
+function handleTimeEntry(timeEntryString) {
+	if (isValidTimeEntry(timeEntryString)){
+		var timeEntry = parseTimeEntry(timeEntryString);
+		say ("OK, recorded that " + timeEntry.employee + " worked " + timeEntry.hours + " at " + timeEntry.client + " on " + timeEntry.date);
+	}
+}
+
+// This should always have three parts - client, hours, date. Requires that currentCall.callerID is a known employee number.
+function parseTimeEntry(timeEntryString) {
+	var parts = timeEntryString.split(" ");
+	var parsedClient = parts[0];
+	var parsedHours = parseFloat(parts[1]);
+	var parsedDate = parseDate(parts[2]);
+	
+	return { 
+		employee: lookupEmployee(),
+		hours: parsedHours,
+		client: parsedClient,
+		date: parsedDate
+	}
+}
+
+function isValidClient(clientName) {
+	return clients.contains(clientName);
+}
+
+function isValidHours(hours) {
+	return (parseFloat(hours) != NaN);
+}
+
+function isValidDate(dateString) {
+	return (parseDate(dateString) != null);
+}
+
+// parseDate can handle two kinds of dates - either 'today' or mm-dd.
+// It returns a full javascript Date object that is set to current time on 
+// the date specified in the current year, or null if it is not a valid date.
+function parseDate(dateString) {
+	var parts = dateString.split("-");
+	if (parts.length < 1 || parts.length > 2) {
+		return null;
+	}
+	if (parts.length == 1) {
+		if (parts[0] == "today") {
+			return new Date();
+		}
+	}
+	if (parts.length == 2) {
+		var monthPart = parts[0];
+		var dayPart = parts[1];
+		if (parseInt(monthPart) == NaN || parseInt(dayPart) == NaN) {
+			return null
+		}
+		var finalDate = new Date();
+		
+		finalDate.setFullYear(finalDate.getFullYear(),parseInt(monthPart),parseInt(dayPart));
+		return finalDate;
+	}
+	return null;
+}
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
 // MAIN
 answer();
 
 log ("########################################## receiving " + currentCall.channel + " call from " + currentCall.callerID);
 
-var worker = lookupWorker(currentCall.callerID);
+var employee = lookupEmployee();
 
-if (worker == null) {
-	worker = ask ("Welcome to the Fertile Ground Time Tracker. What is your name?",
-		{
-		choices: listOptions(workers),
-		timeout: 90.0
-		});
-}
-var flowMethod = ask("Hi " + worker + ". There are two ways to interact with this system by SMS. You can use 1) the interview method or 2) the direct entry method.",{
-	choices: "1,2",
-	timeout: 90.0
+if (employee == null) {
+	employee = ask ("Welcome to the Fertile Ground Time Tracker. I don't recognize your phone number. Reply with your name to register.",
+	{
+		choices: "[ANY]",
+		timeout: 90.0,
+		onChoice: function (event) {
+			handleRegistration(event.value);
+		}
 	});
-if (flowMethod.value == "1") {
-	handleTextInterview();
-} else if (flowMethod.value == "2") {
-	handleTextDirectInput();
+} else {
+	handleKnownEmployee(employee);
 }
